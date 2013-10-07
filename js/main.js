bfac569b66009d50415b9e2168784061905a5453
Micah Ernst <micahwave@gmail.com>
2013-10-07 11:06:28 -0400

    Major updates to css and styles

---
+++ js/main.js
@@ -0,0 +1,149 @@
+(function($){
+	
+	$.fn.postFinder = function(options) {
+
+		var self = this, 
+			$self = $(this),
+			$field = $self.find('input[type=hidden]'),
+			$select = $self.find('select'),
+			$list = $self.find('.list'),
+			$search = $self.find('.search'),
+			$results = $search.find('.results'),
+			$query = $search.find('.input[type=text]'),
+			nonce = $('#post_finder_nonce').val(),
+			template = [
+				'<li data-id="<%= id %>">',
+					'<span><%= title %></span>',
+					'<nav>',
+						'<a href="#" target="_blank">Edit</a>',
+						'<a href="#" target="_blank">View</a>',
+						'<a href="#" class="remove">Remove</a>',
+					'</nav>',
+				'</li>'
+			].join('');
+
+		/**
+		 *
+		 */
+		init = function() {
+
+			// bind select
+			$select.change(function(){
+				add_item( $(this).val(), $('option:selected', this).text() );
+			});
+
+			// bind search button
+			$search.find('.button').click(function(){
+				search();
+			});
+
+			// bind list
+			$list.sortable({
+				placeholder: 'placeholder',
+				update: function(ui, e) {
+					serialize();
+				}
+			});
+
+			// remove button
+			$list.on('click', '.remove', function(e){
+				e.preventDefault();
+				remove_item( $(this).closest('li').data('id'); );
+			});
+
+			// add button
+			$results.on('click', '.add', function(e){
+				e.preventDefault();
+				$li = $(this).closest('li');
+				add_item( $li.data('id'), $li.find('span').text() );
+			});
+		}
+
+		/**
+		 *
+		 */
+		add_item = function( id, title ) {
+
+			// make sure we have an id
+			if( id == 0 )
+				return;
+
+			// see if item already exists
+			if( $list.find('li[data-id="' + id + '"]').length ) {
+				alert('Sorry, that item has already been added.');
+				return;
+			}
+
+			// add item
+			$list.append(_.template(template, { id: id, title: title }));
+
+			// remove from select if there
+			$select.find('option[value="' + id + '"]').remove();
+
+			// update the input
+		}
+
+		/**
+		 *
+		 */
+		remove_item = function( id ) {
+			$list.find('li[data-id="' + id + '"]').remove();
+		}
+
+		/**
+		 *
+		 */
+		search = function() {
+
+			var html = '',
+				data = {
+				action: 'pf_search_posts',
+				s: $query.val(),
+				_ajax_nonce: nonce
+			}
+
+			// display loading
+			$search.addClass('loading');
+
+			$.getJSON(
+				ajaxurl,
+				data,
+				function(response) {
+					if( typeof response.posts != "undefined" ) {
+						for( var i in response.posts ) {
+							html += _.template([
+								'<li data-id="<%= ID %>">',
+									'<a href="#" class="add">Add</a>',
+									'<span><%= post_title %></span>',
+								'</li>'
+							].join(''), response.posts[i]);
+						}
+						$results.html(html);
+					}
+				}
+			);
+		}
+
+		/**
+		 *
+		 */
+		serialize = function() {
+
+			var ids = [];
+
+			$list.find('li').each(function(){
+				ids.push( $(this).data('id') );
+			});
+
+			$field.val( ids.join(',') );
+		}
+
+		// get this started
+		init();
+	}
+
+})(jQuery);
+
+jQuery(document).ready(function($){
+	$('.post-finder').postFinder();
+});
--- post-finder.php
+++ post-finder.php
@@ -12,43 +12,26 @@ if( !class_exists( 'Post_Finder' ) ) :
 class Post_Finder {
 	
 	/**
+	 * Setup hooks
 	 *
+	 * @return void
 	 */
 	function __construct() {
-
-		add_action( 'admin_enqueue_scripts', array( $this, 'admin_js' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'scripts' ) );
 		add_action( 'admin_footer', array( $this, 'admin_footer' ) );
-		add_action( 'wp_ajax_pf_get_posts', array( $this, 'ajax_get_posts' ) );
-		add_action( 'wp_ajax_pf_search_posts', array( $this, 'ajax_search_posts' ) );
-
-		add_action( 'print_media_templates', array( $this, 'print_media_templates' ) );
-
-		add_action( 'edit_form_advanced', array( $this, 'test') );
-	}
-
-	function test() {
-		$this->create_finder( 'my_test', '' );
+		add_action( 'wp_ajax_pf_search_posts', array( $this, 'search_posts' ) );
 	}
 
 	/**
-	 * Adds a new template for the HelloWorld view.
-	 */
-	public static function print_media_templates() {
-		?>
-		<script type="text/html" id="tmpl-hello-world">
-			<strong>Hello World!!!</strong>
-		</script>
-		<?php
-	}
-	
-	/**
+	 * Enable our scripts and stylesheets
 	 *
+	 * @return void
 	 */
-	function admin_js() {
+	function scripts() {
 
 		wp_enqueue_script(
 			'post-finder',
-			plugins_url( '/js/post-finder.js', __FILE__ ),
+			plugins_url( '/js/main.js', __FILE__ ),
 			array(
 				'jquery',
 				'jquery-ui-draggable',
@@ -59,154 +42,163 @@ class Post_Finder {
 		);
 
 		wp_enqueue_style( 'post-finder', plugins_url( '/css/screen.css', __FILE__ ) );
-
-		//wp_localize_script( 'post-finder', 'post_finder_params', array( 'admin_url' => admin_url() ) );
 	}
 	
 	/**
+	 * Make sure our nonce is on all admin pages
 	 *
+	 * @return void
 	 */
 	function admin_footer() {
-		wp_nonce_field( 'post-finder', 'pf-nonce' );
-		?>
-		<div style="background:red;padding:4em">
-			<?php Post_Finder::render( 'test_finder', null, array(
-				'limit' => 5,
-				'post_type' => 'post'
+		wp_nonce_field( 'post_finder', 'post_finder_nonce' );
+	}
+
+	/**
+	 * Builds an input that lets the user find and order posts
+	 *
+	 * @param string Name of input
+	 * @param string Expecting comma seperated post ids
+	 * @param array Field options
+	 */
+	public static function render( $name, $value, $options ) {
+
+		global $wp_post_types;
+
+		// check to see if we have query args
+		$args = isset( $options['args'] ) ? $options['args'] : array();
+
+		// setup some defaults
+		$args = wp_parse_args( $args, array(
+			'post_type' => 'post',
+			'posts_per_page' => 10
+		));
+
+		// now that we have a post type, figure out the proper label
+		if( isset( $wp_post_types[$args['post_type']] ) ) {
+			$singular = $wp_post_types[$args['post_type']]->labels->singular_name;
+			$plural = $wp_post_types[$args['post_type']]->labels->name;
+		} else {
+			$singular = 'Post';
+			$plural = 'Posts';
+		}
+		
+		
+		// get current selected posts if we have a value
+		if( !empty( $value ) ) {
+
+			$post_ids = array_map( 'intval', explode( ',', $value ) );
+
+			$posts = get_posts( array(
+				'post__in' => $post_ids,
+				'orderby' => 'post__in'
 			));
-			?>
+		}
+
+		// if we have some ids already, make sure they arent included in the recent posts
+		if( !empty( $post_ids ) ) {
+			$args['post__not_in'] = $post_ids;
+		} 
+
+		// get recent posts
+		$recent_posts = get_posts( apply_filters( 'post_finder_' . $name . '_recent_post_args', $args ) );
+		
+		?>
+		<div class="post-finder" data-args='<?php echo json_encode( $args ); ?>'>
+			<input type="hidden" name="<?php echo esc_attr( $name ); ?>" value="<?php echo esc_attr( $value ); ?>">
+			<ul class="list">
+				<?php
+
+				if( $posts ) {
+					foreach( $posts as $post ) {
+						printf(
+							'<li data-id="%s">' .
+								'<span>%s</span>' .
+								'<nav>' .
+									'<a href="%s" target="_blank">Edit</a>' .
+									'<a href="#" class="remove">Remove</a>' .
+									'<a href="%s" target="_blank">View</a>' .
+								'</nav>' .
+							'</li>',
+							intval( $post->ID ),
+							esc_html( $post->post_title ),
+							get_edit_post_link( $post->ID ),
+							get_the_guid( $post->ID )
+						);
+					}
+				} else {
+					echo '<p class="notice">No ' . esc_html( $plural ) . ' added.</p>';
+				}
+
+				?>
+			</ul>
+
+			<?php if( $recent_posts ) : ?>
+			<h4>Select a Recent <?php echo esc_html( $singular ); ?></h4>
+			<select>
+				<option value="0">Choose a <?php echo esc_html( $singular ); ?></option>
+				<?php foreach( $recent_posts as $post ) : ?>
+				<option value="<?php echo intval( $post->ID ); ?>"><?php echo esc_html( $post->post_title ); ?></option>
+				<?php endforeach; ?>
+			</select>
+			<?php endif; ?>
+		
+			<div class="search">
+				<h4>Search for a <?php echo esc_html( $singular ); ?></h4>
+				<input type="text" placeholder="Enter a term or phrase">
+				<buttton class="button">Search</buttton>
+				<ul class="results"></ul>
+			</div>
 		</div>
 		<?php
 	}
 
 	/**
+	 * Ajax callback to get posts that we may want to ad
 	 *
-	 *
-	 * @param string Input name
-	 * @param string Comma seperated post ids
+	 * @return void
 	 */
-	public static function render( $name, $value, $args ) {
-
-		if( empty( $value ) )
-			//return;
-
-		// sanitize value
-		$post_ids = array_map( 'intval', explode( ',', $value ) );
-
-		if( !count( $post_ids ) )
-			//return;
-
-		$posts = get_posts( array(
-			'posts_per_page' => 10,
-			'post__in' => $post_ids,
-			'orderby' => 'post__in'
-		));
-
-		if( !empty( $value ) ) {
-
-			$posts = get_posts( array(
-				'posts_per_page' => 200,
-				'post__in' => array_map( 'intval', explode( ',', $value ) ),
-				'orderby' => 'post__in'
-			));
-		}
-	
-		$html = '<input type="text" name="' . esc_attr( $input ) . '" value="'. $value . '" class="pf-input"/>';
-		$html .= '<ul>';
-
-		if( !empty( $posts ) ) {
-			self::build_list( $posts );
-		}
-
-		$html .= '</ul>';
-		$html .= '<input type="button" name="" value="Add Posts" class="pf-add button">';
-
-		echo '<div class="pf-list">' . $html . '</div>';
-	}
-
-	/**
-	 *
-	 */
-	public static function build_list( $posts = array() ) {
-
-		$html = '';
-
-		// must have posts to build something
-		if( !count( $posts ) )
-			return $html;
-
+	function search_posts() {
 		
+		check_ajax_referer( 'post_finder_nonce' );
 
-		foreach( $posts as $post ) {
-			$html .= self::get_li( $post );
-		}
-
-		
-
-		return $html;
-	}
-
-	/**
-	 *
-	 */
-	public static function get_li( $post ) {
-		
-		return sprintf(
-			'<li class="pf-item" data-id="%s">' .
-				'%s'.
-				'<nav>' .
-					'<a href="%s" target="_blank" class="edit">Edit</a>' .
-					'<a href="#" class="remove">Remove</a>' .
-					'<a href="#" class="add">Add</a>' .
-				'</nav>' .
-			'</li>',
-			intval( $post->ID ),
-			get_the_title( $post->ID ) . ' = ' . $post->ID,
-			get_edit_post_link( $post->ID )
+		// possible vars we'll except
+		$vars = array(
+			'posts_per_page',
+			'post_status',
+			's',
+			'post_type',
+			'post_parent'
 		);
-	}
 
-	/**
-	 *
-	 */
-	function ajax_get_posts() {
-		
-		//check_ajax_referer( 'pf-nonce' );
-
-		//die('here');
-		
-		$posts = get_posts( array(
+		// make filterable?
+		$args = array(
 			'posts_per_page' => 20,
-			'post__not_in' => array_map( 'intval', explode( ',', $_POST['ids'] ) )
-		));
+			's' => sanitize_text_field( $_REQUEST['query'] ),
+			'post_status' => 'publish'
+			//'post__not_in' => array_map( 'intval', explode( ',', $_REQUEST['ids'] ) )
+		);
 
-		header("Content-type: text/json");
+		$posts = get_posts( $args );
 
-		die( json_encode( array( 'posts' => $posts ) ) );
-		
-	}
-
-	/**
-	 *
-	 */
-	function ajax_search_posts() {
-		
-		//check_ajax_referer( 'pf-nonce' );
-
-		//die('here');
-		
-		$posts = get_posts( array(
-			'posts_per_page' => 20,
-			's' => sanitize_text_field( $_POST['query'] ),
-			'post__not_in' => array_map( 'intval', explode( ',', $_POST['ids'] ) )
-		));
-
-		header("Content-type: text/json");
-
-		die( json_encode( array( 'posts' => $posts ) ) );
+		if( $posts )
+			header("Content-type: text/json");
+			die( json_encode( array( 'posts' => $posts ) ) );
 		
 	}
 }
 new Post_Finder();
 
 endif;
+
+function test_post_finder() {
+	?>
+	<div style="border:1px solid red;padding:4em 400px">
+		<?php Post_Finder::render( 'test_finder', null, array(
+			'limit' => 5,
+			'post_type' => 'post'
+		));
+		?>
+	</div>
+	<?php
+}
+add_action( 'admin_footer', 'test_post_finder' );
