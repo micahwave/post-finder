72a3dd0f7e8837991fe8997335a7b2ecc4377df0
Jeffrey Paul <jeffpaul@hotmail.com>
2020-08-05 16:02:53 -0500

    update version in post-finder.php

--- post-finder.php
+++ post-finder.php
@@ -3,7 +3,7 @@
  * Plugin Name: Post Finder
  * Author: Micah Ernst, 10up
  * Description: Adds a UI for curating and ordering posts
- * Version: 0.3.0
+ * Version: 0.4.0
  */
 
  if ( ! defined( 'ABSPATH' ) ) {
